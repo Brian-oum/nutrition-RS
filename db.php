dacb39693b96167fd0c62c15b37db5fb6f7a9522
Brian-oum <brianobanda08@gmail.com>
2025-02-17 12:14:40 +0300

    This is for database connection

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost"
+$username = "root"
+$password = ""
+$database = "nutrition"
+
+$conn = new mysqli($host, $email, $password, $database);
+
+if ($conn->connect_error){
+    die("Connection failed" .$conn->connect_error);
+}
+?>
