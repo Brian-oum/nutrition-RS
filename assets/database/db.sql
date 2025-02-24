5364a6b4111811b4529b9ea3be59799dade25c82
Brian-oum <brianobanda08@gmail.com>
2025-02-24 21:22:51 +0300

    password reset

--- assets/database/db.sql
+++ assets/database/db.sql
@@ -18,3 +18,10 @@ CREATE TABLE children (
     weight FLOAT NOT NULL,
     height FLOAT NOT NULL
 );
+
+CREATE TABLE password_resets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
