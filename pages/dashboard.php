d718160205d2cea0cb15981371858294d0929daf
Muli4 <mulimaurice730@gmail.com>
2025-02-27 15:58:59 +0300

    removed session

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,14 +1,4 @@
-<?php
-session_start();
 
-// Redirect if not logged in
-if (!isset($_SESSION["user_id"])) {
-    header("Location: ../index.php");
-    exit();
-}
-
-$username = $_SESSION["username"];
-?>
 
 <!DOCTYPE html>
 <html lang="en">
