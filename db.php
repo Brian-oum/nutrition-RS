f91d29e509109852f31953a943a8cde2cb762d0c
Muli4 <mulimaurice730@gmail.com>
2025-02-28 11:54:55 +0300

    changed connection status

--- db.php
+++ db.php
@@ -8,7 +8,7 @@ $dbname = "nutrition_system";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
