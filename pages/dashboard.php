455e43c75a5fd461ab9f4ace06f76a70250a13aa
Muli4 <mauricemuli730@gmail.com>
2025-03-17 10:42:08 +0300

    new

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -33,38 +33,6 @@ $username = isset($_SESSION["username"]) ? $_SESSION["username"] : "Guest";
                 <i class="fas fa-lock"></i><span class="link-text">Change Password</span></a></li>
             <li><a href="logout.php" data-tooltip="Logout">
                 <i class="fas fa-sign-out-alt"></i><span class="link-text">Logout</span></a></li>
-=======
-            <li>
-                <a href="#" onclick="loadContent('details.php')" data-tooltip="Add Child Details">
-                    <i class="fas fa-child"></i>
-                    <span class="link-text">Add Child Details</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" onclick="loadContent('view_meal_plans.php')" data-tooltip="View Meal Plans">
-                    <i class="fas fa-utensils"></i>
-                    <span class="link-text">View Meal Plans</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" onclick="loadContent('track_progress.php')" data-tooltip="Track Progress">
-                    <i class="fas fa-chart-line"></i>
-                    <span class="link-text">Track Progress</span>
-                </a>
-            </li>
-            <li>
-                <a href="update_profile.php" onclick="loadContent('change_username.php')" data-tooltip="Change Username">
-                    <i class="fas fa-user-edit"></i>
-                    <span class="link-text">Change Username</span>
-                </a>
-            </li>
-            <li>
-                <a href="logout.php" data-tooltip="Logout">
-                    <i class="fas fa-sign-out-alt"></i>
-                    <span class="link-text">Logout</span>
-                </a>
-            </li>
->>>>>>> main
         </ul>
     </nav>
 
